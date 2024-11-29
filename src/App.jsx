4e070686f2f1f24adf3e83a05465588c6aa98e18
AndreiSPACEV1 <74835109+AndreiSPACEV1@users.noreply.github.com>
2024-11-29 04:02:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent from './MyComponent'
-import Counter from './Counter'
-import ColorPicker from './ColorPicker'
+import MyComponent from './MyComponent.jsx'
+import Counter from './Counter.jsx'
+import ColorPicker from './ColorPicker.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
